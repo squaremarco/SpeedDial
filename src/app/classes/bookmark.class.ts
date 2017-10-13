6a34a47b6070b11b4908bcb9c386dec0402e5a09
squaremarco <sq@iamsquare.it>
2017-10-13 09:27:51 +0200

    Refactored bookmark class

--- src/app/classes/bookmark.class.ts
+++ src/app/classes/bookmark.class.ts
@@ -1,13 +1,12 @@
 import * as uuid from 'uuid/v4'
 
 export class Bookmark {
-  id:String;
-  title:String;
-  url:String;
-  imageUrl:String;
-
-  constructor(title?:String, url?:String, imageUrl?:String){
-    this.id = uuid().replace(/-/g, '');
+  constructor(
+    public id?: string,
+    public title?: string,
+    public url?: string,
+    public imageUrl?: string) {
+    this.id = id ? id : uuid().replace(/-/g, '');
     this.title = title ? title : '';
     this.url = url ? url : '';
     this.imageUrl = imageUrl ? imageUrl : '';
